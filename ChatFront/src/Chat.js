7a7d6e6c470e194bd2f424273697549d6f8d918a
Plouvier <LPL3839@viseo.net>
2018-05-25 16:25:35 +0200

    rename folder front

---
+++ ChatFront/src/Chat.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './Chat.css'
+import Post from './Post';
+
+class Chat extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userName: "Léo",
+      avatar: "https://i.pinimg.com/736x/e1/c3/71/e1c371ed831a3c6b5699cce1b3c6cf0f--kitten.jpg",
+      shouldUpdate: true
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+
+    this.setState({ userName: this.refs.txt.value, avatar: this.refs.select.value });
+
+  }
+
+
+  render() {
+
+
+
+    return (
+      <div className="App">
+        <div className="header-container">
+          <header className="header">
+            <h1 className="appTitle">Best Chat EVER</h1>
+            <form className='inputUser'>
+              <input ref='txt' type='text' className="inputUserName" value={this.state.userName} onChange={this.handleChange} />
+              <select ref='select' value={this.state.avatar} onChange={this.handleChange}>
+                <option value="https://i.pinimg.com/736x/e1/c3/71/e1c371ed831a3c6b5699cce1b3c6cf0f--kitten.jpg">Avatar 1</option>
+                <option value="https://avatarfiles.alphacoders.com/547/54795.jpg">Avatar 2</option>
+                <option value="https://pbs.twimg.com/profile_images/552618565199597568/lMgcK3xR.jpeg">Avatar 3</option>
+              </select>
+            </form>
+          </header>
+        </div>
+        <Post userName={this.state.userName} avatar={this.state.avatar} />
+      </div>
+    );
+  }
+}
+
+export default Chat;
---
+++ ChatFront/src/Post.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import './Chat.css'
+import axios from 'axios'
+import Message from './Message.js'
+
+
+class Post extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            AllData: [],
+            NbMessages: 0,
+            AllMessages: []
+        }
+        this.submitMessage = this.submitMessage.bind(this)
+        this.handleKeypress = this.handleKeypress.bind(this)
+        this.update = this.update.bind(this)
+    }
+
+    componentWillMount() {
+        this.update()
+    }
+    componentWillUpdate() {
+        this.update()
+    }
+
+    submitMessage(e) {
+
+
+        if (this.refs.txtInput.value === '') {
+            alert("Le message que vous essayez d'envoyer est vide")
+        }
+        else {
+
+            axios.post('http://localhost:56090/api/Messages', {
+                content: this.refs.txtInput.value,
+                userName: this.props.userName,
+                avatarSrc: this.props.avatar
+
+            })
+                .then(response => console.log('success'))
+                .catch(error => console.log(error))
+
+
+            this.refs.txtInput.value = ''
+
+
+        }
+        this.update()
+
+    }
+
+    handleKeypress(e) {
+        const enterKeyCode = 13
+        if (e.keyCode === enterKeyCode && e.shiftKey === false) {
+            this.refs.txtInput.value = this.refs.txtInput.value.substring(0, this.refs.txtInput.value.length - 1)
+            this.submitMessage()
+        }
+
+    }
+    
+    async update() {
+
+
+        var messages = await axios.get('http://localhost:56090/api/Messages')
+        var mappedMsg = messages.data.messages.map(m =>
+            <div>
+            <Message key={m.id} img={m.avatarSrc}
+                name={m.userName} message={m.content} date={m.date} /><div className="spaceBetween"></div></div>)
+        this.setState({ AllMessages: mappedMsg })
+
+    }
+
+
+    render() {
+
+        return (
+
+            <div>
+                <div className='viewMessage' >
+                    {this.state.AllMessages}
+                </div>
+                <div className='postMessages' >
+                    <textarea ref='txtInput' className='inputText' placeholder="Ecrivez un message..." onKeyUp={this.handleKeypress}></textarea>
+                    <button type='submit' className='submitButton' onClick={this.submitMessage} >Envoyer</button>
+                    <div id="postResult"></div>
+                </div>
+            </div>
+
+        );
+    }
+}
+
+export default Post
